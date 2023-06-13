2a897d204f9536258b129612a62bbc440b2e8fa7
JerPatterson <jeremie.patterson@polymtl.ca>
2023-06-13 08:42:16 -0400

    add get methods for calendar entities

--- src/static/controllers/service/service.controller.ts
+++ src/static/controllers/service/service.controller.ts
@@ -1,6 +1,7 @@
 import {
   Body,
   Controller,
+  Get,
   HttpException,
   HttpStatus,
   Param,
@@ -13,13 +14,31 @@ import { CalendarDateDto, CalendarDto } from 'src/static/utils/dtos';
 export class ServiceController {
   constructor(private serviceService: ServiceService) {}
 
+  @Get('/calendar/:agencyId')
+  async getCalendar(@Param('agencyId') agencyId: string) {
+    try {
+      return await this.serviceService.getCalendar(agencyId);
+    } catch {
+      throw new HttpException('Bad Request', HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  @Get('/calendar/dates/:agencyId')
+  async getCalendarDates(@Param('agencyId') agencyId: string) {
+    try {
+      return await this.serviceService.getCalendarDates(agencyId);
+    } catch {
+      throw new HttpException('Bad Request', HttpStatus.BAD_REQUEST);
+    }
+  }
+
   @Put('/calendar/:agencyId')
   async updateCalendar(
     @Param('agencyId') agencyId: string,
     @Body() calendar: CalendarDto,
   ) {
     try {
-      await this.serviceService.updateCalendar(agencyId, calendar);
+      return await this.serviceService.updateCalendar(agencyId, calendar);
     } catch {
       throw new HttpException('Bad Request', HttpStatus.BAD_REQUEST);
     }
@@ -31,7 +50,10 @@ export class ServiceController {
     @Body() calendarDate: CalendarDateDto,
   ) {
     try {
-      await this.serviceService.updateCalendarDate(agencyId, calendarDate);
+      return await this.serviceService.updateCalendarDate(
+        agencyId,
+        calendarDate,
+      );
     } catch {
       throw new HttpException('Bad Request', HttpStatus.BAD_REQUEST);
     }
--- src/static/services/service/service.service.ts
+++ src/static/services/service/service.service.ts
@@ -5,6 +5,14 @@ import { CalendarDateDto, CalendarDto } from 'src/static/utils/dtos';
 
 @Injectable()
 export class ServiceService {
+  async getCalendar(agencyId: string) {
+    return Calendar.find({ where: { agency_id: agencyId } });
+  }
+
+  async getCalendarDates(agencyId: string) {
+    return CalendarDate.find({ where: { agency_id: agencyId } });
+  }
+
   async updateCalendar(agencyId: string, calendarDto: CalendarDto) {
     const calendar = Calendar.create({ ...calendarDto, agency_id: agencyId });
     return Calendar.save(calendar);
