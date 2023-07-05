dc1df2e9008209cddc45c694805eca55e4463b12
JerPatterson <jeremie.patterson@polymtl.ca>
2023-07-05 09:19:21 -0400

    change the route for trip update request

--- src/realtime/controllers/trip-update/trip-update.controller.ts
+++ src/realtime/controllers/trip-update/trip-update.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+} from '@nestjs/common';
+import { TripUpdateService } from 'src/realtime/services/trip-update/trip-update.service';
 
-@Controller('trip-update')
-export class TripUpdateController {}
+@Controller('trips/update/')
+export class TripUpdateController {
+  constructor(private tripUpdateService: TripUpdateService) {}
+
+  @Get('/:agencyId/:tripId')
+  async getTripUpdateById(
+    @Param('agencyId') agencyId: string,
+    @Param('tripId') tripId: string,
+  ) {
+    try {
+      return await this.tripUpdateService.getTripUpdateById(agencyId, tripId);
+    } catch {
+      throw new HttpException('Bad Request', HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  @Get('/stop/:agencyId/:stopId')
+  async getTripUpdatesFromStop(
+    @Param('agencyId') agencyId: string,
+    @Param('stopId') stopId: string,
+  ) {
+    try {
+      return await this.tripUpdateService.getTripUpdatesFromStop(
+        agencyId,
+        stopId,
+      );
+    } catch {
+      throw new HttpException('Bad Request', HttpStatus.BAD_REQUEST);
+    }
+  }
+}
--- src/realtime/services/trip-update/trip-update.service.ts
+++ src/realtime/services/trip-update/trip-update.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { FeedManagerFactoryService } from 'src/realtime/services/feed-factory/feed-manager-factory.service';
+import GtfsRealtimeBindings from 'gtfs-realtime-bindings';
 
 @Injectable()
-export class TripUpdateService {}
+export class TripUpdateService {
+  constructor(private feedManagerFactoryService: FeedManagerFactoryService) {}
+
+  async getTripUpdateById(
+    agencyId: string,
+    tripId: string,
+  ): Promise<GtfsRealtimeBindings.transit_realtime.IVehiclePosition> {
+    return this.feedManagerFactoryService
+      .getFeedManagerByAgencyId(agencyId)
+      ?.getTripUpdateById(tripId);
+  }
+
+  async getTripUpdatesFromStop(
+    agencyId: string,
+    stopId: string,
+  ): Promise<GtfsRealtimeBindings.transit_realtime.IVehiclePosition[]> {
+    return this.feedManagerFactoryService
+      .getFeedManagerByAgencyId(agencyId)
+      ?.getTripUpdateFromStop(stopId);
+  }
+}
